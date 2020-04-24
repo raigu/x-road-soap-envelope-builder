d9519956cd0e7600ea1143f3966774b5969af95d
Rait Kapp <3442567+raigu@users.noreply.github.com>
2020-04-24 10:14:45 +0300

    Implement building of valid SOAP envelope

--- src/SoapEnvelopeBuilder.php
+++ src/SoapEnvelopeBuilder.php
@@ -8,7 +8,48 @@ final class SoapEnvelopeBuilder
 {
     public function build(): StreamInterface
     {
-        return Stream::empty();
+        //$service = explode('/', $this->service);
+        $service = ['EE', 'COM', '00000000', 'SYS', 'method', 'v0'];
+
+        // source of response sample: https://www.x-tee.ee/docs/live/xroad/pr-mess_x-road_message_protocol.html#e1-request
+        $body = <<<EOD
+<?xml version="1.0" encoding="UTF-8"?>
+<SOAP-ENV:Envelope
+        xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"
+        xmlns:ns1="http://producer.x-road.eu"
+        xmlns:xrd="http://x-road.eu/xsd/xroad.xsd"
+        xmlns:id="http://x-road.eu/xsd/identifiers">
+    <SOAP-ENV:Header>
+        <xrd:client id:objectType="SUBSYSTEM">
+            <id:xRoadInstance>EE</id:xRoadInstance>
+            <id:memberClass>GOV</id:memberClass>
+            <id:memberCode>MEMBER1</id:memberCode>
+            <id:subsystemCode>SUBSYSTEM1</id:subsystemCode>
+        </xrd:client>
+        <xrd:service id:objectType="SERVICE">
+            <id:xRoadInstance>{$service[0]}</id:xRoadInstance>
+            <id:memberClass>{$service[1]}</id:memberClass>
+            <id:memberCode>{$service[2]}</id:memberCode>
+            <id:subsystemCode>{$service[3]}</id:subsystemCode>
+            <id:serviceCode>{$service[4]}</id:serviceCode>
+            <id:serviceVersion>{$service[5]}</id:serviceVersion>
+        </xrd:service>
+        <xrd:id>4894e35d-bf0f-44a6-867a-8e51f1daa7e0</xrd:id>
+        <xrd:userId>EE12345678901</xrd:userId>
+        <xrd:issue>12345</xrd:issue>
+        <xrd:protocolVersion>4.0</xrd:protocolVersion>
+    </SOAP-ENV:Header>
+    <SOAP-ENV:Body>
+        <ns1:exampleService>
+            <exampleInput>foo</exampleInput>
+        </ns1:exampleService>
+    </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+EOD;
+
+        return Stream::fromStr(
+            $body
+        );
     }
 
     public static function create(): self
--- tests/SoapEnvelopeBuilderTest.php
+++ tests/SoapEnvelopeBuilderTest.php
@@ -15,4 +15,18 @@ final class SoapEnvelopeBuilderTest extends TestCase
             SoapEnvelopeBuilder::create()->build()
         );
     }
+
+    /**
+     * @test
+     */
+    public function builds_instance_containing_valid_SOAP_envelope_xml()
+    {
+        $envelope = SoapEnvelopeBuilder::create()->build();
+
+        $dom = new \DOMDocument;
+        $dom->loadXML(strval($envelope));
+        $this->assertTrue(
+            $dom->schemaValidate(__DIR__ . '/soap-v1.1.xsd')
+        );
+    }
 }
