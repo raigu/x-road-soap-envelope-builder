ef74b664b9e97bcebe3139192d98af2d25ad0d90
Rait Kapp <3442567+raigu@users.noreply.github.com>
2020-04-25 15:46:17 +0300

    Add validation for user id

--- src/UserId.php
+++ src/UserId.php
@@ -3,13 +3,18 @@
 namespace Raigu\XRoad;
 
 use DOMDocument;
+use PHPUnit\Runner\Exception;
 
 class UserId implements XmlInjectable
 {
     /**
      * @var string
      */
-    private $value;
+    private $countryCode;
+    /**
+     * @var string
+     */
+    private $personCode;
 
     public function inject(DOMDocument $dom)
     {
@@ -22,18 +27,31 @@ class UserId implements XmlInjectable
             $dom->createElementNS(
                 'http://x-road.eu/xsd/xroad.xsd',
                 'userId',
-                $this->value
+                $this->countryCode . $this->personCode
             )
         );
     }
 
     public static function fromStr(string $value): self
     {
-        return new self($value);
+        return new self(
+            substr($value, 0, 2),
+            substr($value, 2)
+        );
     }
 
-    protected function __construct(string $value)
+    protected function __construct(string $countryCode, string $personCode)
     {
-        $this->value = $value;
+        if (preg_match('/^[A-Z]{2}$/', $countryCode) !== 1) {
+            throw new Exception(
+                sprintf(
+                    'Invalid country code prefix in userId. Must be two-letter ISO country code. Actual: %s',
+                    $countryCode
+                )
+            );
+        }
+
+        $this->countryCode = $countryCode;
+        $this->personCode = $personCode;
     }
 }
---
+++ tests/UserIdValidationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Raigu\XRoad\UserId;
+
+class UserIdValidationTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function fromStr_throws_exception_if_country_code_prefix_missing()
+    {
+        $this->expectExceptionMessage('country code');
+        UserId::fromStr('00000000000');
+    }
+}
