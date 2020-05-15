d65265f4b5a10cb772668bda1deaa4f40970cca3
Rait Kapp <3442567+raigu@users.noreply.github.com>
2020-05-15 13:50:25 +0300

    Add XML element injection

---
+++ src/Element/ElementInjection.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Raigu\XRoad\SoapEnvelope\Element;
+
+use DOMDocument;
+use DOMElement;
+
+final class ElementInjection implements XmlInjectable
+{
+    /**
+     * @var string
+     */
+    private $parentNS;
+    /**
+     * @var string
+     */
+    private $parentTagName;
+    /**
+     * @var DOMElement
+     */
+    private $child;
+
+    public function inject(DOMDocument $dom)
+    {
+        $elements = $dom->getElementsByTagNameNS($this->parentNS, $this->parentTagName);
+        $elements->item(0)->appendChild($this->child);
+    }
+
+    public function __construct(string $parentNS, string $parentTagName, DOMElement $child)
+    {
+        $this->parentNS = $parentNS;
+        $this->parentTagName = $parentTagName;
+        $this->child = $child;
+    }
+}
